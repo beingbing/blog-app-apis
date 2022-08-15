18e1390f93bcd4c2813538bb8f3b4684890007b9
Samar Shaikh <samarshaikh@SamarShaikh.local>
2022-08-15 13:44:57 +0530

    creating posts API having two parent resources

---
+++ src/main/java/com/blog/services/PostService.java
@@ -0,0 +1,32 @@
+package com.blog.services;
+
+import java.util.List;
+
+import com.blog.payloads.PostDto;
+
+public interface PostService {
+	
+	// create
+	PostDto createPost(PostDto postDto, Integer userId, Integer catId);
+	
+	// update
+	PostDto updatePost(PostDto postDto, Integer postId);
+	
+	// delete
+	void deletePost(Integer postId);
+	
+	// get all posts
+	List<PostDto> getAllPosts();
+	
+	// get single post by Id
+	PostDto getPostById(Integer postId);
+	
+	// get all post by category
+	List<PostDto> getPostsByCategory(Integer catId);
+	
+	// get all post by user
+	List<PostDto> getPostsByUser(Integer userId);
+	
+	// get searched posts
+	List<PostDto> searchPosts(String keyword);
+}
